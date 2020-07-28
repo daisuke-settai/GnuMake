aaf7c98f5299b951f6c3a6c2d3a3fd6d72806ad1
ed <ed@inl.ics.keio.ac.jp>
2020-07-29 01:07:09 +0900

    02_C_VPATH

---
+++ 02/C/src/count_words.c
@@ -0,0 +1,9 @@
+#include <counter.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+  int counts[4];
+  counter(counts);
+  printf("%d %d %d %d\n", counts[0], counts[1], counts[2], counts[3]);
+  return 0;
+}
