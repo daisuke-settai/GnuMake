bae9419f68712d31fe8570bb8934d8ce6be8e2b9
ed <ed@inl.ics.keio.ac.jp>
2020-08-04 00:00:52 +0900

    05_B コマンドの評価

---
+++ 05_Command/B/src/B.java
@@ -0,0 +1,9 @@
+public class B{
+  public static void main(String[] args){
+    System.out.println("Hello, world from B.");
+  }
+
+  public static void hello(){
+    System.out.println("Hello, world.");
+  }
+}
